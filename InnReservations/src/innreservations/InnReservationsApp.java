46f53622f2d38cde3702808c5caf88e727b05670
Robert Mullikin <razorknife@gmail.com>
2011-11-27 11:32:40 -0800

    Initial Commit of UI

---
+++ InnReservations/src/innreservations/InnReservationsApp.java
@@ -0,0 +1,44 @@
+/*
+ * InnReservationsApp.java
+ */
+
+package innreservations;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class InnReservationsApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new InnReservationsView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of InnReservationsApp
+     */
+    public static InnReservationsApp getApplication() {
+        return Application.getInstance(InnReservationsApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(InnReservationsApp.class, args);
+    }
+}
